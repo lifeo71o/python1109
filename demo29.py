69d60681ba51216dc421292b88df778fe5c677fe
lifelai <cmjjjjo71o@gmail.com>
2019-11-16 17:02:14 +0800

    1109final

---
+++ demo29.py
@@ -0,0 +1,6 @@
+
+import tensorflow as tf
+
+hello1 = tf.constant('hello tensorflow from python3.7')
+print(tf.__version__)
+print(hello1)
---
+++ demo35.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+
+@tf.function
+def computeArea(sides):
+    a = sides[:, 0]
+    b = sides[:, 1]
+    c = sides[:, 2]
+    s = (a + b + c) / 2
+    areaSquare = s * (s - a) * (s - b) * (s - c)
+    return areaSquare ** 0.5
+
+
+print(computeArea(tf.constant([[5.0, 3.0, 4.0], [4.2, 5.2, 6.2]])))
